a3b289277781bdc003f44e5ece8e4a14581fa3e6
Koby Kramer <koby.e.kramer@gmail.com>
2020-03-29 14:10:28 -0700

    Added Unit Tests to cover Business Logic to ensure that it doesnt break.

---
+++ Steps.md
@@ -0,0 +1,10 @@
+# My thoughts and steps as I approach the problem.
+## Initial Thoughts
+My initial thoughts are that this is spahgetti code and easily doesn't follow SOLID priniciples. The criteria that the Item class cannot be touched is there, but that does not mean that the class cannot be extended following Open/Closed prinicple. We would also want to make the code more readable since readable code is much more important than run speed for smaller application (There is a limit on this idea, but regardless.) We would also want to shift the Items into a Hashmap to enhance the lookup speed, as well as redesign the update quality to call a method of the new class for each method. The question is do we use a custom class for each object or do we use an interface and extend that?
+
+## Steps
+1. Understand the business logic that we're trying to replicate.
+2. Add more units test to ensure we don't break said business logic.
+3. Extend the Item class to hold category for special logic. 
+4. Refactor the Update Quantity to make more ledgeable and convert to Hashmap for easier lookups.
+5. 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,14 +3,74 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void checkFirstItemName() {
         GildedRose app = new GildedRose();
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("+5 Dexterity Vest", app.items[0].name);
     }
 
+    @Test
+    void checkThirdItemUpdatedQuality() {
+        GildedRose app = new GildedRose();
+        app.updateQuality();
+        assertEquals(6, app.items[2].quality);
+    }
+
+    @Test
+    void checkSulfurasStaticQuality() {
+        GildedRose app = new GildedRose();
+        app.updateQuality();
+        assertEquals(80, app.items[3].quality);
+    }
+
+    @Test
+    void checkAgedBrieQualityIncreased() {
+        GildedRose app = new GildedRose();
+        app.updateQuality();
+        assertTrue(app.items[1].quality > 0);
+    }
+
+    @Test
+    void checkSellInValueDecreased() {
+        GildedRose app = new GildedRose();
+        app.updateQuality();
+        assertTrue(app.items[1].sellIn < 10);
+    }
+    
+    @Test  
+    void checkBackstagePassSellIn10QualityIncease() {
+        GildedRose app = new GildedRose();
+        app.items[4].sellIn = 10;
+        app.updateQuality();
+        assertEquals(22,app.items[4].quality);
+    }
+
+    @Test  
+    void checkBackstagePassSellIn5QualityIncease() {
+        GildedRose app = new GildedRose();
+        app.items[4].sellIn = 5;
+        app.updateQuality();
+        assertEquals(23,app.items[4].quality);
+    }
+
+    @Test
+    void checkBackstagePassQualityZerosOut() {
+        GildedRose app = new GildedRose();
+        app.items[4].sellIn = 0;
+        app.updateQuality();
+        assertEquals(0, app.items[4].quality);
+    }
+    
+    @Test
+    void checkQualityDegradesTwiceAfterSellIn() {
+        GildedRose app = new GildedRose();
+        app.items[0].sellIn = -1;
+        app.updateQuality();
+        assertEquals(18, app.items[0].quality);
+    }
 }
